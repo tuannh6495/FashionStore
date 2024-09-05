1aff831fd4ca1f53805b031baa23497cee3345ab
datt2606 <DatTong.work@gmail.com>
2024-09-05 10:20:37 +0700

    FOS-7 <Chưa xong homepage>

---
+++ src/FashionStore.WebApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace XTLAB
+{
+    public class Product
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public string Price { get; set; }
+    }
+}
