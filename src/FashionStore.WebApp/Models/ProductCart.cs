0bb0d1fcc58c89b17eafc33fcf6f93d8f79b3a2d
Quý Nguyễn Đức <ngducquy104203@gmail.com>
2024-09-20 11:47:14 +0700

    FOS-24 <da xong reponsive >

---
+++ src/FashionStore.WebApp/Models/ProductCart.cs
@@ -0,0 +1,12 @@
+﻿namespace FashionStore.WebApp.Models
+{
+	public class ProductCart
+	{
+		public string? Name { get; set; }
+		public string? Size { get; set; }
+		public string? Color { get; set; }
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }
+		public string? ImagePath { get; set; }
+	}
+}
---
+++ src/FashionStore.WebApp/Pages/Shared/Components/ProductCart/CartItemViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FashionStore.WebApp.Models;
+using System.Collections.Generic;
+
+namespace FashionStore.WebApp.ViewComponents
+{
+	public class CartItemViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke(List<ProductCart> products)
+		{
+			// Đảm bảo rằng tên view trùng khớp với tên thư mục và file bạn đã tạo
+			return View("~/Pages/Shared/Components/ProductCart/Default.cshtml", products);
+		}
+	}
+}
